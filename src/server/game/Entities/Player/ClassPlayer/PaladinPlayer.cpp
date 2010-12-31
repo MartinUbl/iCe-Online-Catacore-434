35d54a2bce5c4d9e8169fe8d2f49494c254115fe
cmaranec <cmaranec@seznam.cz>
2010-12-31 23:01:14 +0100

    Core/Players: Implement base holy power handling for PaladinPlayer class

--- src/server/game/Entities/Player/ClassPlayer/PaladinPlayer.cpp
+++ src/server/game/Entities/Player/ClassPlayer/PaladinPlayer.cpp
@@ -1,3 +1,21 @@
 #include "PaladinPlayer.h"
 
-//...
+PaladinPlayer::PaladinPlayer(WorldSession* session): Player(session)
+{
+    SetMaxPower(POWER_HOLY_POWER,3);
+    SetHolyPower(0);
+}
+
+uint8 PaladinPlayer::GetHolyPower()
+{
+    uint8 power = GetPower(POWER_HOLY_POWER);
+    return power > 3 ? 3 : power;
+}
+
+void PaladinPlayer::SetHolyPower(uint8 value)
+{
+    if(value > 3)
+        SetPower(POWER_HOLY_POWER,3);
+    else
+        SetPower(POWER_HOLY_POWER,value);
+}
