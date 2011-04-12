2fac49c2d1d59b478e6038cf656768116366ff96
cmaranec <cmaranec@seznam.cz>
2011-04-12 20:59:29 +0200

    Core/Taxi: allow being taxicheater only for GMs

--- src/server/game/Server/Protocol/Handlers/TaxiHandler.cpp
+++ src/server/game/Server/Protocol/Handlers/TaxiHandler.cpp
@@ -114,7 +114,7 @@ void WorldSession::SendTaxiMenu(Creature* unit)
     data << uint64(unit->GetGUID());
     data << uint32(curloc);
     data << uint8((sTaxiNodesStore.GetNumRows() >> 6) + 1); // This is 11 as of 4.0.6 13623 (count of following uint64's)
-    GetPlayer()->m_taxi.AppendTaximaskTo(data,GetPlayer()->isTaxiCheater());
+    GetPlayer()->m_taxi.AppendTaximaskTo(data,GetPlayer()->isGameMaster()?GetPlayer()->isTaxiCheater():false);
     SendPacket(&data);
 
 	sLog.outDebug("WORLD: Sent SMSG_SHOWTAXINODES");
