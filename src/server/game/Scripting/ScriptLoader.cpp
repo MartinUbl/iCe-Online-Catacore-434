d12753e0c4b077cff3b0c41af5a8d3f1c868bab7
Cerey <thyros82@gmx.de>
2011-04-24 03:30:27 +0200

    Core/Scripts: add example_commandscript as an example of how to use commandscripts to add your own commands to the core
    by Paradox.

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -30,6 +30,7 @@ void AddSC_example_creature();
 void AddSC_example_escort();
 void AddSC_example_gossip_codebox();
 void AddSC_example_misc();
+void AddSC_example_commandscript();
 
 // spells
 void AddSC_deathknight_spell_scripts();
@@ -581,6 +582,7 @@ void AddExampleScripts()
     AddSC_example_escort();
     AddSC_example_gossip_codebox();
     AddSC_example_misc();
+    AddSC_example_commandscript();
 }
 
 void AddSpellScripts()
