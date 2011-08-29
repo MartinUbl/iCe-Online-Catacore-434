7b91656592d855160fd3584dced7e796913679a5
cmaranec <cmaranec@seznam.cz>
2011-08-29 19:48:18 +0200

    Scripts/ThroneOfTheFourWinds: Alakir spells enum, still incomplete

--- src/server/scripts/Kalimdor/TheSkywall/ThroneOfTheFourWinds/boss_alakir.cpp
+++ src/server/scripts/Kalimdor/TheSkywall/ThroneOfTheFourWinds/boss_alakir.cpp
@@ -19,6 +19,30 @@
 #include "ScriptPCH.h"
 #include "throne_of_the_four_winds.h"
 
+enum Stuff
+{
+    // phase 1
+    SPELL_LIGHTNING_STRIKE_DAMAGE = 88214,
+    SPELL_LIGHTNING_STRIKE_VISUAL = 88230,
+    SPELL_STATIC_SHOCK            = 87873,
+    SPELL_ICE_STORM_TRIGGER       = 87469,
+    SPELL_ICE_STORM_SUMMON        = 87055,
+    SPELL_SQUALL_LINE_VEHICLE     = 87856,
+    SPELL_WIND_BURST              = 87770,
+    // phase 2
+    SPELL_ACID_RAIN_TRIGGER       = 88290,
+    SPELL_ACID_RAIN_DOT           = 88301,
+    NPC_STORMLING                 = 47175,
+    SPELL_FEEDBACK                = 87904, // casted after stormling death
+    // phase 3
+    SPELL_EYE_OF_THE_STORM        = 82724,
+    SPELL_RELENTLESS_STORM_VEHICLE= 89104,
+    SPELL_RELENTLESS_STORM_VISUAL = 88866, // visual effect, is this used in encounter?
+    NPC_RELENTLESS_STORM          = 47807, // used for lighning?
+    SPELL_LIGHTNING_ROD           = 89667,
+    // lightning clouds?
+};
+
 void AddSC_alakir()
 {
 }
