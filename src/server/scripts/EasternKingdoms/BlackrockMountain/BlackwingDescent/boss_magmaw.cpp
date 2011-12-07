a41b956e62ae545ca4ee20a44dafd767416751be
cmaranec <cmaranec@seznam.cz>
2011-12-07 01:54:21 +0100

    Scripts/BlackwingDescent: Magmaw - more work

--- src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingDescent/boss_magmaw.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingDescent/boss_magmaw.cpp
@@ -21,12 +21,17 @@
 
 enum Spells
 {
-    SPELL_MAGMA_SPIT = 78068,
+    SPELL_MAGMA_SPIT = 78359,
+    SPELL_MAGMA_SPIT_WITH_ENRAGE = 78068,
     SPELL_LAVA_SPEW = 77690,
-    SPELL_PILLAR_OF_FLAME_TRIGGER_AURA = 78006,
-    SPELL_PILLAR_OF_FLAME_TRIGGERED = 78010,
-    SPELL_PILLAR_OF_FLAME_THROW = 77971,
+    SPELL_PILLAR_OF_FLAME_EXPLOSION = 77970, // 4 seconds after spawning
+    SPELL_PILLAR_OF_FLAME_VISUAL = 78017, //flaming circle
     SPELL_MANGLE = 89773,
+    SPELL_IMPALE_SELF = 77907, // used for exposing self
+    SPELL_LAVA_SPLASH_VISUAL = 79461,
+
+    SPELL_POINT_OF_VULNERABILITY_SHARE_DMG = 79010,
+    SPELL_POINT_OF_VULNERABILITY = 79011,
 
     // Lava Parasite
     SPELL_PARASITIC_INFECTION_1 = 78941,
@@ -51,14 +56,83 @@ public:
     {
         boss_magmawAI(Creature* c): Scripted_NoMovementAI(c)
         {
+            // Aby nam nespadnul
             me->SetUnitMovementFlags(MOVEMENTFLAG_CAN_FLY | MOVEMENTFLAG_FLYING);
             Reset();
+            if (pHead = me->SummonCreature(42347, 0,0,0))
+            {
+                me->CastSpell(pHead, SPELL_POINT_OF_VULNERABILITY_SHARE_DMG, true);
+                pHead->EnterVehicle(me, 1, false);
+                pHead->addUnitState(UNIT_STAT_UNATTACKABLE);
+                pHead->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE | UNIT_FLAG_NOT_SELECTABLE);
+            }
         }
+
+        uint32 MagmaSpitTimer;
+        uint32 LavaSpewTimer;
+        uint32 PillarOfFlameTimer;
+        Creature* pHead;
+        uint32 DeExposeTimer;
 
         void Reset()
         {
             // Just for being sure that we arrive at homepoint even visual
             me->GetMotionMaster()->MovePoint(0, me->GetPositionX(), me->GetPositionY(), me->GetPositionZ());
+        }
+
+        void EnterCombat(Unit* pWho)
+        {
+            MagmaSpitTimer = 15000;
+            LavaSpewTimer = 12000;
+            PillarOfFlameTimer = 35000;
+            DeExposeTimer = 0;
+        }
+
+        void JustDied(Unit* pWho)
+        {
+            if (pHead)
+            {
+                pHead->ExitVehicle();
+                //pHead->Relocate(0,0,0);
+                pWho->Kill(pHead);
+            }
+        }
+
+        void UpdateAI(const uint32 diff)
+        {
+            if (!UpdateVictim())
+                return;
+
+            if (DeExposeTimer == 0)
+            {
+                if (MagmaSpitTimer <= diff)
+                {
+                    me->CastSpell(me->getVictim(), SPELL_MAGMA_SPIT, false);
+                    MagmaSpitTimer = urand(12000, 15000);
+                }
+                else
+                    MagmaSpitTimer -= diff;
+
+                if (LavaSpewTimer <= diff)
+                {
+                    me->CastSpell(me, SPELL_LAVA_SPEW, false);
+                    LavaSpewTimer = urand(12000, 15000);
+                }
+                else
+                    LavaSpewTimer -= diff;
+
+                if (PillarOfFlameTimer <= diff)
+                {
+                    Unit* pRandom = SelectUnit(SELECT_TARGET_RANDOM, 0);
+                    if (pRandom)
+                        me->SummonCreature(NPC_PILLAR_OF_FLAME, pRandom->GetPositionX(), pRandom->GetPositionY(), pRandom->GetPositionZ(), 0, TEMPSUMMON_TIMED_DESPAWN, 8500);
+                    PillarOfFlameTimer = urand(32000, 35000);
+                }
+                else
+                    PillarOfFlameTimer -= diff;
+            }
+
+            DoMeleeAttackIfReady();
         }
     };
 
@@ -68,14 +142,65 @@ public:
     }
 };
 
+class mob_pillar_of_flame_magmaw: public CreatureScript
+{
+public:
+    mob_pillar_of_flame_magmaw(): CreatureScript("mob_pillar_of_flame_magmaw") {};
+
+    struct mob_pillar_of_flame_magmawAI: public Scripted_NoMovementAI
+    {
+        mob_pillar_of_flame_magmawAI(Creature* c): Scripted_NoMovementAI(c)
+        {
+            Reset();
+        }
+
+        uint32 ExplodeTimer;
+        uint32 UnAuraTimer;
+
+        void Reset()
+        {
+            ExplodeTimer = 4000;
+            UnAuraTimer = 7500;
+            me->CastSpell(me, SPELL_PILLAR_OF_FLAME_VISUAL, false);
+        }
+
+        void UpdateAI(const uint32 diff)
+        {
+            if (ExplodeTimer <= diff)
+            {
+                me->CastSpell(me, SPELL_PILLAR_OF_FLAME_EXPLOSION, false);
+                ExplodeTimer = 20000;
+            }
+            else
+                ExplodeTimer -= diff;
+
+            if (UnAuraTimer <= diff)
+            {
+                me->RemoveAurasDueToSpell(SPELL_PILLAR_OF_FLAME_VISUAL);
+                UnAuraTimer = 20000;
+            }
+            else
+                UnAuraTimer -= diff;
+        }
+    };
+
+    CreatureAI* GetAI(Creature* c) const
+    {
+        return new mob_pillar_of_flame_magmawAI(c);
+    }
+};
+
 void AddSC_magmaw()
 {
     new boss_magmaw();
+    new mob_pillar_of_flame_magmaw();
 }
 
 /**** SQL:
 
-UPDATE creature_template SET ScriptName='boss_magmaw' WHERE entry=41570;
-REPLACE INTO `creature_model_info` (`modelid`, `bounding_radius`, `combat_reach`, `gender`, `modelid_other_gender`) VALUES (32679, 8, 0, 2, 0);
+UPDATE creature_template SET vehicleId=781,ScriptName='boss_magmaw' WHERE entry=41570;
+UPDATE creature_template SET modelid1=16946,modelid3=0,unit_flags=2,faction_A=14,faction_H=14,ScriptName='mob_pillar_of_flame_magmaw' WHERE entry=41843;
+REPLACE INTO `creature_model_info` (`modelid`, `bounding_radius`, `combat_reach`, `gender`, `modelid_other_gender`) VALUES (32679, 15, 15, 2, 0);
+DELETE FROM creature_template_addon WHERE entry=42347;
 
 */
