44849d0dda6d14921e707cbf90ce448821d01cab
cmaranec <cmaranec@seznam.cz>
2011-12-21 22:47:04 +0100

    Core/Spells: prevent indefinite looping in Judgements of the Wise handling
    Damn SpellIconID checks! From Judgements of the Wise aura spell (those 31930) proc'd also talent Communition, which has, obviously, the same icon ID as Judgements of the Wise talent, so the handler proc'd another 31930, and so on..

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -6816,7 +6816,7 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, AuraEffect* trigger
                 return false;
             }
             // Judgements of the Wise
-            if (dummySpell->SpellIconID == 3017)
+            if (dummySpell->Id == 31878)
             {
                 target = this;
                 triggered_spell_id = 31930;
