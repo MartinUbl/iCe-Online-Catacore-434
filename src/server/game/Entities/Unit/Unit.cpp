608be95dfaec2a9c3f5bec87285b5f114d5b8167
cmaranec <cmaranec@seznam.cz>
2011-12-29 20:08:08 +0100

    Core/Unit: we don't need these asserts
    It wouldn't bring any inconsistences, memory corruptions or something like that. It would only probably break gameplay of one person, not thousands as with assert.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -221,14 +221,6 @@ Unit::~Unit()
 
     ASSERT(!m_duringRemoveFromWorld);
     ASSERT(!m_attacking);
-    ASSERT(m_attackers.empty());
-    ASSERT(m_sharedVision.empty());
-    ASSERT(m_Controlled.empty());
-    ASSERT(m_appliedAuras.empty());
-    ASSERT(m_ownedAuras.empty());
-    ASSERT(m_removedAuras.empty());
-    ASSERT(m_gameObj.empty());
-    ASSERT(m_dynObj.empty());
 }
 
 void Unit::Update(uint32 p_time)
