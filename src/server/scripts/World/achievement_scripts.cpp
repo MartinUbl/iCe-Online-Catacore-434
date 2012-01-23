8c4766acdb44fd40702213bbc7f28cd440510cdc
cmaranec <cmaranec@seznam.cz>
2012-01-23 18:44:07 +0100

    Scripts/Achievements: import some Love is in the Air stuff
    Flirt With Disaster
    I pitied the fool
    Lonely?

--- src/server/scripts/World/achievement_scripts.cpp
+++ src/server/scripts/World/achievement_scripts.cpp
@@ -223,6 +223,156 @@ class achievement_bg_ic_mowed_down : public AchievementCriteriaScript
 //        }
 //};
 
+class go_romantic_picnic: public GameObjectScript
+{
+public:
+    go_romantic_picnic(): GameObjectScript("go_romantic_picnic") {}
+
+    bool OnGossipHello(Player* pPlayer, GameObject* pObject)
+    {
+        pPlayer->CastSpell(pPlayer,45123,true);
+        pPlayer->SetStandState(UNIT_STAND_STATE_SIT);
+
+        if(Unit* pOwner = pObject->GetOwner())
+        {
+            Player* pPlayerOwner = (Player*)pOwner;
+            if(pPlayerOwner != pPlayer && pPlayer->HasAura(71074) && pPlayerOwner->HasAura(71074))
+            {
+                const AchievementEntry* achiev = sAchievementStore.LookupEntry(1291);
+                if(achiev)
+                {
+                    pPlayerOwner->CompletedAchievement(achiev);
+                    pPlayer->CompletedAchievement(achiev);
+                }
+            }
+        }
+        else
+            return false;
+
+        return true;
+    }
+};
+
+class npc_love_fool: public CreatureScript
+{
+public:
+    npc_love_fool(): CreatureScript("npc_love_fool") {}
+
+    enum
+    {
+        CRITERIA_WINTERGRASP             = 6343,
+        CRITERIA_GURUBASHI_ARENA         = 6344,
+        CRITERIA_ARATHI_BASIN_BLACKSMITH = 6345,
+        CRITERIA_CULLING_OF_STRATHOLME   = 6346,
+        CRITERIA_NAXXRAMAS               = 6347
+    };
+
+    struct love_foolAI: public PassiveAI
+    {
+        love_foolAI(Creature* c): PassiveAI(c) { }
+
+        void ReceiveEmote(Player* pPlayer, uint32 text_emote)
+        {
+            if(text_emote == TEXTEMOTE_PITY)
+            {
+                if(pPlayer->GetAreaId() == 3421)
+                    pPlayer->GetAchievementMgr().SetCriteriaProgress(CRITERIA_ARATHI_BASIN_BLACKSMITH, 1704, 1, PROGRESS_SET);
+                if(pPlayer->GetZoneId() == 4197)
+                    pPlayer->GetAchievementMgr().SetCriteriaProgress(CRITERIA_WINTERGRASP, 1704, 1, PROGRESS_SET);
+                if(pPlayer->GetMapId() == 595)
+                    pPlayer->GetAchievementMgr().SetCriteriaProgress(CRITERIA_CULLING_OF_STRATHOLME, 1704, 1, PROGRESS_SET);
+                if(pPlayer->GetAreaId() == 2177)
+                    pPlayer->GetAchievementMgr().SetCriteriaProgress(CRITERIA_GURUBASHI_ARENA, 1704, 1, PROGRESS_SET);
+                if(pPlayer->GetMapId() == 533)
+                    pPlayer->GetAchievementMgr().SetCriteriaProgress(CRITERIA_NAXXRAMAS, 1704, 1, PROGRESS_SET);
+            }
+        }
+    };
+
+    CreatureAI* GetAI(Creature* pCreature) const
+    {
+        return new love_foolAI(pCreature);
+    }
+};
+
+const uint32 PerfumeIds[] = {70234, 70235, 70233, 71507, 26682};
+
+class npc_sraaz_or_jeremiah: public CreatureScript
+{
+public:
+    npc_sraaz_or_jeremiah(): CreatureScript("npc_sraaz_or_jeremiah") {}
+
+    enum
+    {
+        CRITERIA_KISS_SRAAZ = 3931,
+        CRITERIA_KISS_JEREMIAH = 3929,
+        CRITERIA_HANDFUL_ON_SRAAZ = 12859,
+        CRITERIA_HANDFUL_ON_JEREMIAH = 4227,
+        ACHIEVEMENT_FLIRT_WITH_DISASTER_A = 1279,
+        ACHIEVEMENT_FLIRT_WITH_DISASTER_H = 1280
+    };
+
+    struct sharedAI: public ScriptedAI
+    {
+        sharedAI(Creature* c): ScriptedAI(c)
+        {
+            if(me->GetEntry() == 8403) //Jeremiah
+            {
+                CriteriaKiss = CRITERIA_KISS_JEREMIAH;
+                CriteriaHandful = CRITERIA_HANDFUL_ON_JEREMIAH;
+                AchievementId = ACHIEVEMENT_FLIRT_WITH_DISASTER_H;
+            }
+            else if(me->GetEntry() == 9099) //Sraaz
+            {
+                CriteriaKiss = CRITERIA_KISS_SRAAZ;
+                CriteriaHandful = CRITERIA_HANDFUL_ON_SRAAZ;
+                AchievementId = ACHIEVEMENT_FLIRT_WITH_DISASTER_A;
+            }
+        }
+
+        uint32 CriteriaKiss;
+        uint32 CriteriaHandful;
+        uint32 AchievementId;
+
+        bool HavePerfume(Player* pPlayer)
+        {
+            for(int i = 0; i < (sizeof(PerfumeIds)/sizeof(uint32)); i++)
+            {
+                if(pPlayer->HasAura(PerfumeIds[i]))
+                    return true;
+            }
+            return false;
+        }
+
+        void ReceiveEmote(Player* pPlayer, uint32 text_emote)
+        {
+            if(pPlayer->GetDrunkValue() < 0xFFF || !HavePerfume(pPlayer))
+                return;
+
+            if(text_emote == TEXTEMOTE_KISS)
+            {
+                pPlayer->GetAchievementMgr().SetCriteriaProgress(CriteriaKiss, AchievementId, 1, PROGRESS_SET);
+            }
+        }
+
+        void SpellHit(Unit* caster, const SpellEntry* spell)
+        {
+            Player* pPlayer = (Player*)caster;
+
+            if(!pPlayer || pPlayer->GetDrunkValue() < 0xFFF || !HavePerfume(pPlayer))
+                return;
+
+            if(spell->Id == 27571)
+                pPlayer->GetAchievementMgr().SetCriteriaProgress(CriteriaHandful, AchievementId, 1, PROGRESS_SET);
+        }
+    };
+
+    CreatureAI* GetAI(Creature* pCreature) const
+    {
+        return new sharedAI(pCreature);
+    }
+};
+
 void AddSC_achievement_scripts()
 {
     new achievement_school_of_hard_knocks();
@@ -236,4 +386,7 @@ void AddSC_achievement_scripts()
 //    new achievement_bg_sa_artillery_veteran();
 //    new achievement_bg_sa_artillery_expert();
 //    new achievement_bg_sa_drop_it();
+    new go_romantic_picnic();
+    new npc_sraaz_or_jeremiah();
+    new npc_love_fool();
 }
