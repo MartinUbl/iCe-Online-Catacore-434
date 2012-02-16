c8c7b299c6493973fda93ec65c6b6a46328e3931
freghar <compmancz@gmail.com>
2012-02-16 17:56:26 +0100

    git repository cleanup of unused stuff

--- sql/ice/character_cata_banned.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `character_cata_banned` (
-`guid` int(11) NOT NULL,
-  PRIMARY KEY (`guid`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
--- sql/ice/character_stats.sql
+++
@@ -1,3 +0,0 @@
-
-ALTER TABLE `character_stats` ADD COLUMN `resilience` int(10) unsigned NOT NULL DEFAULT '0' AFTER `spellPower`;
-ALTER TABLE `character_stats` ADD COLUMN `achievPoints` int(10) unsigned NOT NULL DEFAULT '0' AFTER `resilience`;
