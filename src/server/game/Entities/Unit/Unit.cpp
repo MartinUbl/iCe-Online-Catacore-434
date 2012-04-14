069e1b29eb8e4eac9e19e774ac9c8e94b337483e
cmaranec <cmaranec@seznam.cz>
2012-04-14 20:49:47 +0200

    Core/Spells: fix logic in auratype 343 handling

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -12491,9 +12491,12 @@ void Unit::MeleeDamageBonus(Unit *pVictim, uint32 *pdamage, WeaponAttackType att
     // SPELL_AURA_MOD_AUTOATTACK_DAMAGE_1, SPELL_AURA_MOD_AUTOATTACK_DAMAGE_2
     if (!spellProto)
     {
-        AuraEffectList const & autoattackDamage = GetAuraEffectsByType(SPELL_AURA_MOD_AUTOATTACK_DAMAGE_1);
-        for (AuraEffectList::const_iterator i = autoattackDamage.begin(); i != autoattackDamage.end(); ++i)
-            AddPctN(DoneTotalMod, (*i)->GetAmount());
+        if (pVictim)
+        {
+            AuraEffectList const & autoattackDamage = pVictim->GetAuraEffectsByType(SPELL_AURA_MOD_AUTOATTACK_DAMAGE_1);
+            for (AuraEffectList::const_iterator i = autoattackDamage.begin(); i != autoattackDamage.end(); ++i)
+                AddPctN(DoneTotalMod, (*i)->GetAmount());
+        }
 
         AuraEffectList const & autoattackDamage2 = GetAuraEffectsByType(SPELL_AURA_MOD_AUTOATTACK_DAMAGE_2);
         for (AuraEffectList::const_iterator i = autoattackDamage2.begin(); i != autoattackDamage2.end(); ++i)
