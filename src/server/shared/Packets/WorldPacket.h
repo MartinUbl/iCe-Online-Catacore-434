05663315285983958bc430f27b4e8e8870438a93
cmaranec <cmaranec@seznam.cz>
2012-08-04 00:04:16 +0200

    Core/Packets: added mechanism for saving real opcode number in WorldPacket

--- src/server/shared/Packets/WorldPacket.h
+++ src/server/shared/Packets/WorldPacket.h
@@ -34,19 +34,21 @@ class WorldPacket : public ByteBuffer
         {
         }
         explicit WorldPacket(uint32 opcode, size_t res=200, bool hack = false) : ByteBuffer(res), m_opcode(opcode)
-		{
-			// This is a hack fix.
-			// The client will not 'eat' certain opcodes when they're sent under normal circumstances.
-			// This is due to the new redirection system implemented by Blizzard in Cataclysm.
-			// Another solution to get them handled would be to patch the client even heavier than we already do.
-			// In this case, I choose to spread hacks in our code, rather than in Blizzard's
-			// - Dvlpr
+        {
+            // This is a hack fix.
+            // The client will not 'eat' certain opcodes when they're sent under normal circumstances.
+            // This is due to the new redirection system implemented by Blizzard in Cataclysm.
+            // Another solution to get them handled would be to patch the client even heavier than we already do.
+            // In this case, I choose to spread hacks in our code, rather than in Blizzard's
+            // - Dvlpr
             if (hack)
             {
                 m_opcode = SMSG_MULTIPLE_PACKETS;
                 *this << uint16(opcode);
             }
-		}
+
+            m_realOpcode = opcode;
+        }
                                                             // copy constructor
         WorldPacket(const WorldPacket &packet)              : ByteBuffer(packet), m_opcode(packet.m_opcode)
         {
@@ -63,14 +65,19 @@ class WorldPacket : public ByteBuffer
                 *this << uint16(opcode);
             }
             else m_opcode = opcode;
+
+            m_realOpcode = opcode;
         }
 
         uint32 GetOpcode() const { return m_opcode; }
-        void SetOpcode(uint32 opcode) { m_opcode = opcode; }
+        void SetOpcode(uint32 opcode) { m_opcode = opcode; m_realOpcode = opcode; }
+
+        uint32 GetRealOpcode() { return m_realOpcode; }
 
     protected:
 
         uint32 m_opcode;
+        uint32 m_realOpcode;
 };
 #endif
 
