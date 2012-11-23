6fc3843d0729c0457f729666dd33673f24083098
HyN3Q <h.hyneq@gmail.com>
2012-11-23 18:27:57 +0100

    Scripts/BWD: my mistake - missing script loader for Maloriak

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -574,6 +574,7 @@ void AddSC_instance_throne_of_the_four_winds();
 void AddSC_boss_argaloth();
 void AddSC_instance_baradin_hold();
 void AddSC_magmaw();
+void AddSC_maloriak();
 void AddSC_atramedes();
 void AddSC_boss_defensing_system();
 void AddSC_instance_blackwing_descent();
@@ -1203,6 +1204,7 @@ void AddCataclysmScripts()
     AddSC_twilight_council();
 
     AddSC_magmaw();
+    AddSC_maloriak();
     AddSC_atramedes();
     AddSC_boss_defensing_system();
     AddSC_instance_blackwing_descent();
