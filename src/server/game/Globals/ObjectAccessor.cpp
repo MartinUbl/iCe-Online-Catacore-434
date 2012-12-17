e79f16fe8c5d63d03ee06877aef58adc96d03d80
HyN3Q <h.hyneq@gmail.com>
2012-12-17 16:01:05 +0100

    Core/Objects: fixed build error

--- src/server/game/Globals/ObjectAccessor.cpp
+++ src/server/game/Globals/ObjectAccessor.cpp
@@ -298,7 +298,7 @@ Corpse* ObjectAccessor::ConvertCorpseForPlayer(uint64 player_guid, bool insignia
     // ignore bones creating option in case insignia
 
     if (map && (insignia ||
-        (map->IsBattlegroundOrArena() ? sWorld->getIntConfig(CONFIG_DEATH_BONES_BG_OR_ARENA) : sWorld->getIntConfig(CONFIG_DEATH_BONES_WORLD))) &&
+        (map->IsBattlegroundOrArena() ? sWorld->getBoolConfig(CONFIG_DEATH_BONES_BG_OR_ARENA) : sWorld->getBoolConfig(CONFIG_DEATH_BONES_WORLD))) &&
         !map->IsRemovalGrid(corpse->GetPositionX(), corpse->GetPositionY()))
     {
         // Create bones, don't change Corpse
