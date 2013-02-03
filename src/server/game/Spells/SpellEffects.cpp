8b8861fb46ef5d3b355d3ab956dc591a66b5325f
Arti <unknown@unknown.cz>
2013-02-03 01:07:32 +0100

    Core/Spells: druid - Infected Wounds also proc from Ravage! (Stampede)

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -6382,6 +6382,12 @@ void Spell::SpellDamageWeaponDmg(SpellEffIndex effIndex)
                 m_caster->RemoveAurasDueToSpell(89140); // enabler spell
                 m_caster->RemoveAurasDueToSpell(81021); // Stampede buff rank 1
                 m_caster->RemoveAurasDueToSpell(81022); // Stampede buff rank 2
+
+                if (m_caster->HasAura(48483)) // Infected Wounds (Rank 1)
+                    m_caster->CastSpell(unitTarget, 58179, true);
+                else if (m_caster->HasAura(48484)) // Infected Wounds (Rank 2)
+                    m_caster->CastSpell(unitTarget, 58180, true);
+
             }
             else if (m_spellInfo->Id == 6785) // Ravage
             {
