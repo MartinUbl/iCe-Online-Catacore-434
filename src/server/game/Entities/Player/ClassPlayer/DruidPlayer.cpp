e9c11c930642b578f526d11002654e469181ce9b
Aliquis <majosp2@gmail.com>
2013-03-14 00:23:31 +0100

    Revert "Core/Units: speeded-up searching for power index for class (from TC)"
    This reverts commit 8836f849a4f973e87a49218cc538d2327e566b99.

--- src/server/game/Entities/Player/ClassPlayer/DruidPlayer.cpp
+++ src/server/game/Entities/Player/ClassPlayer/DruidPlayer.cpp
@@ -57,7 +57,7 @@ void DruidPlayer::ClearEclipseState()
     RemoveAurasDueToSpell(48518);
 
     // and hardly set eclipse power to zero
-    uint32 powerIndex = GetPowerIndex(POWER_ECLIPSE);
+    uint32 powerIndex = GetPowerIndexByClass(POWER_ECLIPSE, getClass());
     if (powerIndex != MAX_POWERS)
         SetUInt32Value(UNIT_FIELD_POWER1 + powerIndex, 0);
 }
