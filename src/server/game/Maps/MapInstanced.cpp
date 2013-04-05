f39ae641fbc2fb386233395eb3388a5557184e30
Aliquis <majosp2@gmail.com>
2013-04-05 07:34:53 +0200

    Revert "core/instance:Flexible id- few major changes in system"
    This reverts commit bb2fa6e46b20d9e559185a99ad8530606e27dcda.

--- src/server/game/Maps/MapInstanced.cpp
+++ src/server/game/Maps/MapInstanced.cpp
@@ -119,9 +119,7 @@ Map* MapInstanced::CreateInstance(const uint32 mapId, Player * player)
 {
     if (GetId() != mapId || !player)
         return NULL;
-    if(player->GetSession()->GetSecurity()==SEC_PLAYER)
-        player->_LoadBoundInst(); //reset of temporary instance bounds for flexible id
- 
+
     Map* map = NULL;
     uint32 NewInstanceId = 0;                       // instanceId of the resulting map
 
@@ -154,11 +152,11 @@ Map* MapInstanced::CreateInstance(const uint32 mapId, Player * player)
                     pSave = groupBind->save;
             }
         }
-        //instance bound merging when same bosses killed(flexible id)
+        //instance bound merging when same bosses killed
         Group *group = player->GetGroup();
         if (player->GetSession()->GetSecurity()==SEC_PLAYER && group && pBind && pBind->perm &&
             group->GetBoundInstance(this) && group->GetLeader() &&
-            pBind->save != group->GetBoundInstance(this)->save) 
+            pBind->save != group->GetBoundInstance(this)->save)
         {
             Player *leader = group->GetLeader();
 
@@ -181,7 +179,7 @@ Map* MapInstanced::CreateInstance(const uint32 mapId, Player * player)
                 {
                     bool canMerge=true;
                     for (uint32 i = 0; i < count; i++)
-                        if (bossP[i]==DONE&&bossG[i]!=DONE)
+                        if (bossP[i] != bossG[i])
                         {
                             canMerge=false;
                             break;
@@ -193,12 +191,8 @@ Map* MapInstanced::CreateInstance(const uint32 mapId, Player * player)
                         if (groupBind)
                         {
                             pSave = groupBind->save;
-                            pBind->save=groupBind->save;
-                            player->m_boundInstances[pSave->GetDifficulty()][pSave->GetMapId()]=*pBind;
-                            bool hero=true;
-                            if(iGMap->GetDifficulty()==RAID_DIFFICULTY_10MAN_HEROIC||iGMap->GetDifficulty()==RAID_DIFFICULTY_25MAN_HEROIC) // if difficulty is heroic bind instantly, else after killing boss
-                                hero=false;
-                            player->BindToInstance(pSave, true,true, hero);
+                            player->UnbindInstance(pSave->GetMapId(), pSave->GetDifficulty(), true);
+                            player->BindToInstance(pSave, true);
                         }
                      }
                 }
