7a1ad60b486a3e8f176669aad21aa7dca42ad442
cmaranec <cmaranec@seznam.cz>
2013-09-05 21:28:17 +0200

    Core/Player: secure one part of code when checking for HC achievement

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -19615,8 +19615,8 @@ bool Player::Satisfy(AccessRequirement const* ar, uint32 target_map, bool report
         if (ar->achievement && !GetAchievementMgr().HasAchieved(sAchievementStore.LookupEntry(ar->achievement)))
             missingAchievement = ar->achievement;
 
-        if (mapEntry->IsRaid() && ar->achievement && GetGroup()->GetLeader()->GetAchievementMgr().HasAchieved(sAchievementStore.LookupEntry(ar->achievement))) //only leader need to have achievement to access heroic raid
-            missingAchievement=0;
+        if (mapEntry->IsRaid() && ar->achievement && GetGroup() && GetGroup()->GetLeader() && GetGroup()->GetLeader()->GetAchievementMgr().HasAchieved(sAchievementStore.LookupEntry(ar->achievement))) //only leader need to have achievement to access heroic raid
+            missingAchievement = 0;
 
 
         Difficulty target_difficulty = GetDifficulty(mapEntry->IsRaid());
