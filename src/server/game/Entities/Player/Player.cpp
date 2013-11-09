e68551c3de89777c1e9b339d28bc53f817958c00
cmaranec <cmaranec@seznam.cz>
2013-11-09 01:25:48 +0100

    Core/Player: unapply auras when leaving event zone 752

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -7788,6 +7788,12 @@ void Player::UpdateZone(uint32 newZone, uint32 newArea)
         CastSpell(this, 34311, true);
         CastSpell(this, 39811, true);
     }
+    else
+    {
+        RemoveAurasDueToSpell(84594);
+        RemoveAurasDueToSpell(34311);
+        RemoveAurasDueToSpell(39811);
+    }
 
     AreaTableEntry const* zone = GetAreaEntryByAreaID(newZone);
     if (!zone)
