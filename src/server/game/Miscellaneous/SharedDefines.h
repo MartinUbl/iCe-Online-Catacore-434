88042a27eee300753c72470fa9a9294b08c8f564
cmaranec <cmaranec@seznam.cz>
2013-12-22 22:55:15 +0100

    Core/Objects: added new researched gameobject dynamic flag

--- src/server/game/Miscellaneous/SharedDefines.h
+++ src/server/game/Miscellaneous/SharedDefines.h
@@ -1512,6 +1512,7 @@ enum GameObjectDynamicLowFlags
     GO_DYNFLAG_LO_ANIMATE           = 0x02,                 // possibly more distinct animation of GO
     GO_DYNFLAG_LO_NO_INTERACT       = 0x04,                 // appears to disable interaction (not fully verified)
     GO_DYNFLAG_LO_SPARKLE           = 0x08,                 // makes GO sparkle
+    GO_DYNFLAG_LO_STOPPED           = 0x10,                 // flag for stopped transport (or any animated object which supports that)
 };
 
 enum GameObjectDestructibleState
