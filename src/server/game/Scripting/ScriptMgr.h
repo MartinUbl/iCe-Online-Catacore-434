5369a52937e8c6802404933533d824ffd1c6ac4d
cmaranec <cmaranec@seznam.cz>
2014-01-05 22:36:21 +0100

    Core/Scripting: vehicle scripts are database bound scripts
    This allows existence of both CreatureScript and VehicleScript with same
    name at same time

--- src/server/game/Scripting/ScriptMgr.h
+++ src/server/game/Scripting/ScriptMgr.h
@@ -593,6 +593,8 @@ class VehicleScript : public ScriptObject
 
     public:
 
+        bool IsDatabaseBound() const { return true; }
+
         // Called after a vehicle is installed.
         virtual void OnInstall(Vehicle* /*veh*/) { }
 
