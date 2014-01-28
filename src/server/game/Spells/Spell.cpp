27b4d04a0ad517f6cae3046cb0929f39c1e8bce1
Ivan <tourcite@gmail.com>
2014-01-28 16:26:13 +0100

    Core/Spells: hunter - Cobra shot and Chimera shot can't be cast while hunter has detterance

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -5968,6 +5968,13 @@ SpellCastResult Spell::CheckCast(bool strict)
         }
     }
 
+    // Cobra shot and Chimera shot can't be cast while hunter has detterance
+    if (m_spellInfo->Id == 77767 || m_spellInfo->Id == 53209 && m_caster->GetTypeId() == TYPEID_PLAYER)
+    {
+        if (m_caster->HasAuraType(SPELL_AURA_DEFLECT_SPELLS))
+            return SPELL_FAILED_CANT_DO_THAT_RIGHT_NOW;
+    }
+
     // Killing Spree check - at least one enemy in range
     if (m_spellInfo->Id == 51690)
     {
