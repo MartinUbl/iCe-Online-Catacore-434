a57d7da565c65bd6c8fc2f6c79b8ba2c0856865e
cmaranec <cmaranec@seznam.cz>
2014-04-22 21:19:52 +0200

    Core/Spells: hunter - Kill Command - spread the change to lines below

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -6031,7 +6031,7 @@ SpellCastResult Spell::CheckCast(bool strict)
             if (m_caster->getVictim() == NULL) // Hunter must has enemy target
                 return SPELL_FAILED_NO_VALID_TARGETS;
 
-            if (pPet->GetDistance(Target) > 5.0f ) // Pet need to be in 5 yard range
+            if (pPet->GetDistance(m_caster->getVictim()) > 5.0f) // Pet need to be in 5 yard range
                 return SPELL_FAILED_CANT_DO_THAT_RIGHT_NOW;
         }
         else
