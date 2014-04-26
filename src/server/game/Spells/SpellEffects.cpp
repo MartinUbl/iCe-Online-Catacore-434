a8b7cdf9a61f34ae20c2e2de2efa706ee7022a33
Ivan <tourcite@gmail.com>
2014-04-26 18:12:21 +0200

    Core/Spells: DK - Fixed typo in Blood Boil damage calculation.

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -1693,7 +1693,7 @@ void Spell::SpellDamageSchoolDmg(SpellEffIndex effIndex)
                 if (m_spellInfo->SpellFamilyFlags[0] & 0x00040000 && unitTarget->GetAuraEffect(SPELL_AURA_PERIODIC_DAMAGE, SPELLFAMILY_DEATHKNIGHT, 0, 0, 0x00000002, m_caster->GetGUID()))
                 {
                     damage += m_damage / 2;
-                    damage += int32(m_caster->GetTotalAttackPowerValue(RANGED_ATTACK)* 0.035f);
+                    damage += int32(m_caster->GetTotalAttackPowerValue(BASE_ATTACK)* 0.035f);
                 }
                 //// Death Coil - disable adding bonus
                 //else if (m_spellInfo->Id == 47632)
