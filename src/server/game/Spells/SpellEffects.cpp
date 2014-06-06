427f3dc218b10bcfecbbe868f20a79685fab2ed8
Ivan <tourcite@gmail.com>
2014-06-07 01:16:10 +0200

    Core/Spels: priest - Surge of Light should not be consumed by original Flash Heal spell.

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -4591,7 +4591,6 @@ void Spell::SpellDamageHeal(SpellEffIndex effIndex)
         // Flash Heal
         else if (m_spellInfo->Id == 2061 && caster->HasAura(88688))
         {
-            caster->RemoveAurasDueToSpell(88688); // Surge of Light remove
             addhealth = caster->SpellHealingBonus(unitTarget, m_spellInfo, effIndex, addhealth, HEAL);
         }
         // Chain Heal and Riptide
