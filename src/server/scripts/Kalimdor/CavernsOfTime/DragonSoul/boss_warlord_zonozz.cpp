19cb758419f3ca5475af257c9eb4bb8179de7ccc
Ivan <tourcite@gmail.com>
2014-08-07 21:47:44 +0200

    Core/Scripts; DS - Added spell and entry enums for Warlord Zon'ozz encounter.

--- src/server/scripts/Kalimdor/CavernsOfTime/DragonSoul/boss_warlord_zonozz.cpp
+++ src/server/scripts/Kalimdor/CavernsOfTime/DragonSoul/boss_warlord_zonozz.cpp
@@ -18,6 +18,42 @@
 #include "ScriptPCH.h"
 #include "dragonsoul.h"
 
+enum spells
+{
+    /* BOSS SPELLS */
+    SPELL_FOCUSED_ANGER                 = 104543, // OK
+    SPELL_PSYCHIC_DRAIN                 = 104323, // OK
+    SPELL_DISRUPTING_SHADOWS            = 103434, // OK (maybe cast with max affected targets)
+    SPELL_DISRUPTING_SHADOWS_KNOCKBACK  = 103948, // this should be triggered after dispeling spell above
+  //SPELL_BLACK_BLOOD_INFINTY           = 104377, // infinite duration ?
+    SPELL_BLACK_BLOOD                   = 104378, // higher damage + 30 s duration
+    SPELL_VOID_OF_THE_UNMAKING_SUMMON   = 103571, // summon sphere in front of the caster
+    SPELL_DARKNESS                      = 109413, // damn, hate looking up correct spells
+
+    /* VOID SPHERE SPELLS */
+    SPELL_VOID_DIFFUSION_DAMAGE         = 103527, // should split damage
+    SPELL_VOID_DIFFUSION_TAKEN          = 104031, // stackable damage taken aura (TARGET_UNIT_SRC_AREA_ENTRY) -> just use AddAura on boss instead
+    SPELL_VOID_OF_THE_UNMAKING_CHANNEL  = 103946, // dummy channel
+    SPELL_VOID_OF_THE_UNMAKING_VISUAL   = 109187, // visual purple aura (infinite)
+    SPELL_VOID_OF_THE_UNMAKING_REMOVE   = 105336, // should remove aura above
+    SPELL_BLACK_BLOOD_ERUPTION          = 110382, // if sphere hits the edge of the room -> TARGET_UNIT_CASTER -> cast by players themsleves probably
+
+    /* SUMMONS SPELLS*/
+    SPELL_OOZE_SPIT                     = 109396, // Casted by Claw of Go'rath when no one in melee range
+    SPELL_SHADOW_GAZE                   = 104347, // Casted by Eye of Go'rath to random player
+    SPELL_WILD_FLAIL                    = 109199, // Casted be Flail of Go'rath (melee AoE)
+    SPELL_SLUDGE_SPEW                   = 110102, // Casted be Flail of Go'rath to random player
+};
+
+enum entries
+{
+    WARLORD_ENTRY               = 55308,
+    CLAW_OF_GORATH_ENTRY        = 55418,
+    EYE_OF_GORATH_ENTRY         = 57875,
+    FLAIL_OF_GORATH_ENTRY       = 55417,
+    VOID_SPHERE                 = 55334
+};
+
 void AddSC_boss_warlord_zonozz()
 {
 }
