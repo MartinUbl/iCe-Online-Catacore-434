47dc8446905656ce04a46151a65ad5b912e914b0
Aliquis <majosp2@gmail.com>
2014-11-02 13:10:59 +0100

    Core: removed dependence on WorldSocket.h being included first

--- src/server/game/PrecompiledHeaders/gamePCH.h
+++ src/server/game/PrecompiledHeaders/gamePCH.h
@@ -24,7 +24,19 @@
 #ifndef _GAMEPCH_H
 #define _GAMEPCH_H
 #ifdef TRINITY_COREPCH
-#include "WorldSocket.h"        // must be first to make ACE happy with ACE includes in it
+
+#include <ace/Basic_Types.h>
+#include <ace/Synch_Traits.h>
+#include <ace/Svc_Handler.h>
+#include <ace/SOCK_Stream.h>
+#include <ace/SOCK_Acceptor.h>
+#include <ace/Acceptor.h>
+#include <ace/Thread_Mutex.h>
+#include <ace/Guard_T.h>
+#include <ace/Unbounded_Queue.h>
+#include <ace/Message_Block.h>
+
+#include "WorldSocket.h"
 
 #include "Common.h"
 
