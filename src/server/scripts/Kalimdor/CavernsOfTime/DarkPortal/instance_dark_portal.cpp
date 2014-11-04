0f1f1a75fcc51c97eca3d3bb853430b01a9ff7bc
Lazik <LazikL@email.cz>
2014-11-04 01:38:38 +0100

    Scripts/Dark Portal: Decreased timers for rifts

--- src/server/scripts/Kalimdor/CavernsOfTime/DarkPortal/instance_dark_portal.cpp
+++ src/server/scripts/Kalimdor/CavernsOfTime/DarkPortal/instance_dark_portal.cpp
@@ -55,9 +55,9 @@ static Wave RiftWaves[]=
 {
     {RIFT_BOSS, 0},
     {C_DEJA, 0},
-    {RIFT_BOSS, 120000},
-    {C_TEMPO, 140000},
-    {RIFT_BOSS, 120000},
+    {RIFT_BOSS, 45000},
+    {C_TEMPO, 60000},
+    {RIFT_BOSS, 45000},
     {C_AEONUS, 0}
 };
 
