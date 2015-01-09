ba1131312928e221266c5e266d958108cc56e854
cmaranec <cmaranec@seznam.cz>
2015-01-09 11:22:23 +0100

    Core/Transports: disable loading transport NPCs for now

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1856,7 +1856,7 @@ void World::SetInitialWorldSettings()
 
     sLog->outString("Loading Transports...");
     sTransportMgr->SpawnContinentTransports();
-    sTransportMgr->LoadTransportNPCs();
+    //sTransportMgr->LoadTransportNPCs();
 
     sLog->outString("Deleting expired bans...");
     LoginDatabase.Execute("DELETE FROM ip_banned WHERE unbandate <= UNIX_TIMESTAMP() AND unbandate<>bandate");
