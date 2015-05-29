f47bedba59295970f2b3e6c587bc7d1afda840e9
Lazik <LazikL@email.cz>
2015-05-29 20:00:07 +0200

    Revert "Core/Spells: mage - Fixed DR category for Ring of Frost."
    This reverts commit 5f80a8178e2904570c8baa4a837fe82a0ac62638.

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3057,7 +3057,7 @@ DiminishingGroup GetDiminishingReturnsGroupForSpell(SpellEntry const* spellproto
                 return DIMINISHING_CONTROL_ROOT;
             // Ring of Frost
             if (spellproto->Id == 82691)
-                return DIMINISHING_CONTROL_STUN;
+                return DIMINISHING_DISORIENT;
             // Slow
             if (spellproto->Id == 31589)
                 return DIMINISHING_LIMITONLY;
