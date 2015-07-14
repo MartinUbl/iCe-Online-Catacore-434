0ab5a36a2f48948a01ea48ac9ca04a85e543e121
Ivan <tourcite@gmail.com>
2015-07-15 00:01:45 +0200

    Core/Unit: Fixed warning (logical not is only applied to the left hand side of comparison).

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -5361,7 +5361,7 @@ void Unit::AddGameObject(GameObject* gameObj)
 
 void Unit::RemoveGameObject(GameObject* gameObj, bool del)
 {
-    if (!gameObj || !gameObj->GetOwnerGUID() == GetGUID()) return;
+    if (!gameObj || gameObj->GetOwnerGUID() != GetGUID()) return;
 
     gameObj->SetOwnerGUID(0);
 
