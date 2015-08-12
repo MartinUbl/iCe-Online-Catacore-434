15a01b17d58e5ed0b54f90dc728476c2e9ed159b
cmaranec <cmaranec@seznam.cz>
2015-08-12 16:56:38 +0200

    Core/BattlegroundSA: disable pathfinding in Strand of the Ancients

--- src/server/collision/Management/MMapFactory.cpp
+++ src/server/collision/Management/MMapFactory.cpp
@@ -67,7 +67,7 @@ namespace MMAP
 #ifdef _WIN32
         return sWorld->getBoolConfig(CONFIG_ENABLE_MMAPS);
 #else
-        bool isBGMap = (mapId == 30 || mapId == 489 || mapId == 529 || mapId == 566 || mapId == 607 || mapId == 628 || mapId == 726 || mapId == 761);
+        bool isBGMap = (mapId == 30 || mapId == 489 || mapId == 529 || mapId == 566 || mapId == 628 || mapId == 726 || mapId == 761);
 
         return ((mapId == 530 || mapId == 939 || isBGMap) && sWorld->getBoolConfig(CONFIG_ENABLE_MMAPS));
 #endif
