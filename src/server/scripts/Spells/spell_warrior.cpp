8c8e7daf2c2bb62ad650916a401090c95bf0df68
cmaranec <cmaranec@seznam.cz>
2016-02-07 17:25:26 +0100

    Revert "Core/Spells: Fixed Warrior T13 Protection 2P Bonus (Revenge)."
    This reverts commit 8536c9c32e01355fb125aa94236c947556800c49.

--- src/server/scripts/Spells/spell_warrior.cpp
+++ src/server/scripts/Spells/spell_warrior.cpp
@@ -25,9 +25,7 @@
 
 enum WarriorSpells
 {
-    WARRIOR_SPELL_LAST_STAND_TRIGGERED  = 12976,
-    WARRIOR_2P_PROTECTION_SET_BONUS     = 105908,
-    WARRIOR_SPELL_SHIELD_OF_FURY        = 105909
+    WARRIOR_SPELL_LAST_STAND_TRIGGERED           = 12976,
 };
 
 class spell_warr_last_stand : public SpellScriptLoader
@@ -39,69 +37,33 @@ class spell_warr_last_stand : public SpellScriptLoader
         {
             PrepareSpellScript(spell_warr_last_stand_SpellScript)
 
-            bool Validate(SpellEntry const * /*spellEntry*/) override
+            bool Validate(SpellEntry const * /*spellEntry*/)
             {
-                return sSpellStore.LookupEntry(WARRIOR_SPELL_LAST_STAND_TRIGGERED) != nullptr;
+                if (!sSpellStore.LookupEntry(WARRIOR_SPELL_LAST_STAND_TRIGGERED))
+                    return false;
+                return true;
             }
 
             void HandleDummy(SpellEffIndex /*effIndex*/)
             {
                 int32 healthModSpellBasePoints0 = int32(GetCaster()->CountPctFromMaxHealth(30));
-                GetCaster()->CastCustomSpell(GetCaster(), WARRIOR_SPELL_LAST_STAND_TRIGGERED, &healthModSpellBasePoints0, nullptr, nullptr, true, nullptr);
+                GetCaster()->CastCustomSpell(GetCaster(), WARRIOR_SPELL_LAST_STAND_TRIGGERED, &healthModSpellBasePoints0, NULL, NULL, true, NULL);
             }
 
-            void Register() override
+            void Register()
             {
+                // add dummy effect spell handler to Last Stand
                 OnEffect += SpellEffectFn(spell_warr_last_stand_SpellScript::HandleDummy, EFFECT_0, SPELL_EFFECT_DUMMY);
             }
         };
 
-        SpellScript *GetSpellScript() const override
+        SpellScript *GetSpellScript() const
         {
             return new spell_warr_last_stand_SpellScript();
         }
 };
 
-class spell_warr_revenge : public SpellScriptLoader
-{
-public:
-    spell_warr_revenge() : SpellScriptLoader("spell_warr_revenge") { }
-
-    class spell_warr_revenge_SpellScript : public SpellScript
-    {
-        PrepareSpellScript(spell_warr_revenge_SpellScript)
-
-        bool Validate(SpellEntry const * /*spellEntry*/) override
-        {
-            return sSpellStore.LookupEntry(WARRIOR_SPELL_SHIELD_OF_FURY) != nullptr;
-        }
-
-        void HandleRevengeHit(SpellEffIndex /*effIndex*/)
-        {
-            if (Unit * caster = GetCaster())
-            {
-                if (AuraEffect * aurEff = caster->GetAuraEffect(WARRIOR_2P_PROTECTION_SET_BONUS, EFFECT_0))
-                {
-                    int32 bp0 = GetHitDamage() * aurEff->GetAmount() / 100;
-                    caster->CastCustomSpell(caster, WARRIOR_SPELL_SHIELD_OF_FURY, &bp0, nullptr, nullptr, true, nullptr);
-                }
-            }
-        }
-
-        void Register() override
-        {
-            OnEffect += SpellEffectFn(spell_warr_revenge_SpellScript::HandleRevengeHit, EFFECT_0, SPELL_EFFECT_SCHOOL_DAMAGE);
-        }
-    };
-
-    SpellScript *GetSpellScript() const override
-    {
-        return new spell_warr_revenge_SpellScript();
-    }
-};
-
 void AddSC_warrior_spell_scripts()
 {
-    new spell_warr_last_stand();
-    new spell_warr_revenge();   // 6572
+    new spell_warr_last_stand;
 }
