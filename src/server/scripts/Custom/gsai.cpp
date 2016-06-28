e5cb0a9d7fd18685730b727e89261be164102346
cmaranec <cmaranec@seznam.cz>
2016-06-28 10:22:40 +0200

    Core/GScript: "in range" script should consider line of sight

--- src/server/scripts/Custom/gsai.cpp
+++ src/server/scripts/Custom/gsai.cpp
@@ -1019,7 +1019,7 @@ class GS_CreatureScript : public CreatureScript
 
             void MoveInLineOfSight(Unit* who) override
             {
-                if (m_playerInRangeScriptId > -1 && me->GetDistance(who) <= m_playerInRangeScriptDistance)
+                if (m_playerInRangeScriptId > -1 && me->GetDistance(who) <= m_playerInRangeScriptDistance && me->IsWithinLOSInMap(who, false))
                 {
                     if (GS_SelectScript(GS_TYPE_PLAYER_IN_RANGE, (int32)m_playerInRangeScriptDistance, who))
                         ResetState();
